44c6df6530dfba430d835d18101362ce2d0f80ce
lemoer <git@irrelefant.net>
2018-08-18 02:42:23 +0530

    First steps

---
+++ batadv.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python3
+
+from scapy.packet import *
+from scapy.fields import *
+from scapy.all import *
+
+
+class Disney(Packet):
+    name = "DisneyPacket "
+    fields_desc=[ ShortField("mickey",5),
+                 XByteField("minnie",3) ,
+                 IntEnumField("donald" , 1 ,
+                      { 1: "happy", 2: "cool" , 3: "angry" } ) ]
+
+BATADV_PACKET_TYPES = {}
+BATADV_PACKET_TYPES['IV_OGM'] = 0
+
+BATADV_TVLV_TYPES = {}
+BATADV_TVLV_TYPES['DAT'] = 0x02
+BATADV_TVLV_TYPES['TT'] = 0x04
+BATADV_TVLV_TYPES['Multicast'] = 0x06
+
+# TODO: remove "pad" field
+# TODO: default type of Tvlv packet?
+# TODO: solve flags better?
+# TODO: types?
+# TODO: ukn field?
+
+class _FinishedPacket(Packet):
+    # contains no payload
+    def extract_padding(self, p):
+        return b"", p
+
+class _BatAdvTvlvHDR(Packet):
+    fields_desc = [
+        ByteEnumField('type', None, BATADV_TVLV_TYPES), 
+        ByteField('version', 0x01),
+        FieldLenField('length', None, length_of="value")]
+   
+
+class BatAdvTvlv(_FinishedPacket):
+    name = 'BATADV TVLV Packet'
+    fields_desc = [
+        _BatAdvTvlvHDR,
+        StrLenField("value", "", length_from=lambda pkt:pkt.length)]
+
+    registered_ip_options = {}
+
+    @classmethod
+    def register_variant(cls):
+        cls.registered_ip_options[cls.type.default] = cls
+
+    @classmethod
+    def dispatch_hook(cls, pkt=None, *args, **kargs):
+        if pkt:
+            opt = orb(pkt[0])
+            if opt in cls.registered_ip_options:
+                return cls.registered_ip_options[opt]
+        return cls
+
+class BatAdvTvlvTTVLAN(_FinishedPacket):
+    name = 'TT VLAN'
+    fields_desc = [
+        XIntField('crc', 0),
+        XShortField('vid', 0),
+        XShortField('ukn', 0)
+    ]
+
+class BatadvTvlvTTEntry(_FinishedPacket):
+    name = 'TT Entry'
+    fields_desc=[
+        XByteField('flags', 0),
+        BitField('pad', 0, 24),
+        MACField('addr', ETHER_ANY),
+        XShortField('vid', 0)
+    ]
+
+class BatAdvTvlvTT(BatAdvTvlv):
+    name = 'TT Container'
+    type = BATADV_TVLV_TYPES['TT']
+    fields_desc = [
+        _BatAdvTvlvHDR,
+        XByteField('flags', 0),
+        ByteField('ttvn', 0),
+        BitFieldLenField('vlanCount', None, 16, count_of='vlans'),
+        PacketListField('vlans', [], BatAdvTvlvTTVLAN,
+            count_from=lambda pkt: pkt.vlanCount),
+        PacketListField('entries', [], BatadvTvlvTTEntry,
+            length_from=lambda pkt: pkt.length-pkt.vlanCount*8-4)
+        ]
+
+class BatAdvOGM(_FinishedPacket):
+    name = 'OGM'
+    fields_desc=[
+        ByteEnumField('type', 'IV_OGM', BATADV_PACKET_TYPES),
+        ByteField('version', 15),
+        ByteField('ttl', 64),
+        XByteField('flags', 0x00),
+        IntField('seq', 0),
+        MACField('originator', ETHER_ANY),
+        MACField('rcvFrom', ETHER_ANY),
+        ByteField('pad', 0),
+        ByteField('tq', 255),
+        FieldLenField('tvlvLen', None, length_of='tvlvs'),
+        PacketListField("tvlvs", [], BatAdvTvlv, length_from=lambda p:p.tvlvLen)]
+
+class BatAdv(Packet):
+    name = 'BatAdv Packet'
+    fields_desc=[
+        PacketListField('container', [], BatAdvOGM)
+    ]
+
+
+bind_layers(Ether, BatAdv, type=0x4305)
+
+packets = rdpcap('test.pcap')
+packets2 = rdpcap('test2.pcap')
+
+p = packets[0]
+p2 = packets2[0]
+
+p.show2()
+
+
+p2[BatAdv].container[4].show()
