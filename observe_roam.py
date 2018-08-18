b922d2f9a57b4c93e286cbe25a91d2274dd5dc62
lemoer <git@irrelefant.net>
2018-08-18 07:34:32 +0530

    observe_roam.py with tt

--- observe_roam.py
+++ observe_roam.py
@@ -6,37 +6,65 @@ from batadv import *
 
 seen = []
 
-def pkt_callback(pkt):
+def pkt_callback(pkt, mac):
     assert(BatAdv in pkt)
     global seen
 
     y = 0
 
-    for ogm in pkt[BatAdv].container:
-        if BatAdvOGM not in ogm:
-            continue
+    for c in pkt[BatAdv].container:
+        if BatAdvOGM in c:
+            ogm = c
 
-        if ogm.seq in seen:
-            continue
-
-        seen += [ogm.seq]
-
-
-        for tvlv in ogm.tvlvs:
-            if BatAdvTvlvTT not in tvlv:
+            if ogm.seq in seen:
                 continue
 
-            for entry in tvlv.entries:
-                f = entry.flags & 0x03
-                if f == 0:
-                    print('ADD!      - orig {} added   {}'.format(ogm.originator, entry.addr))
-                elif f == 1:
-                    print('DEL!      - orig {} dropped {}'.format(ogm.originator, entry.addr))
-                elif f == 2:
-                    print('ROAM IN!  - orig {} added   {}'.format(ogm.originator, entry.addr))
-                elif f == 3:
-                    print('ROAM OUT! - orig {} dropped {}'.format(ogm.originator, entry.addr))
-                y = 1
+            seen += [ogm.seq]
+
+
+            for tvlv in ogm.tvlvs:
+                if BatAdvTvlvTT not in tvlv:
+                    continue
+
+                for entry in tvlv.entries:
+                    f = entry.flags & 0x03
+                    if f == 0:
+                        print('ADD!      - orig {} added   {}'.format(ogm.originator, entry.addr))
+                    elif f == 1:
+                        print('DEL!      - orig {} dropped {}'.format(ogm.originator, entry.addr))
+                    elif f == 2:
+                        print('ROAM IN!  - orig {} added   {}'.format(ogm.originator, entry.addr))
+                    elif f == 3:
+                        print('ROAM OUT! - orig {} dropped {}'.format(ogm.originator, entry.addr))
+                    y = 1
+        elif BatAdvUnicastTvlv in c:
+            #TODO: fix
+            ogm = c
+            if mac is not None and c.dst != mac:
+                continue
+            for tvlv in c.tvlvs:
+                if BatAdvTvlvTT not in tvlv:
+                    continue
+                tt = tvlv
+
+                if (tt.flags & 0x10) == 0x10:
+                    ft = 'FT'
+                else:
+                    ft = '  '
+
+                for entry in tvlv.entries:
+                    f = entry.flags & 0x03
+                    if f == 0:
+                        print(ft, '2ADD!      - orig {} added   {}'.format(ogm.src, entry.addr))
+                    elif f == 1:
+                        print(ft, '2DEL!      - orig {} dropped {}'.format(ogm.src, entry.addr))
+                    elif f == 2:
+                        print(ft, '2ROAM IN!  - orig {} added   {}'.format(ogm.src, entry.addr))
+                    elif f == 3:
+                        print(ft, '2ROAM OUT! - orig {} dropped {}'.format(ogm.src, entry.addr))
+                    y = 1
+
+
     if y:
         print('.')
 
@@ -44,9 +72,10 @@ def pkt_callback(pkt):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('file', metavar='FILE', type=str)
+    parser.add_argument('--filter', metavar='MAC', type=str)
 
     args = parser.parse_args()
 
     packets = rdpcap(args.file)
     for pkt in packets:
-        pkt_callback(pkt)
+        pkt_callback(pkt, args.filter)
